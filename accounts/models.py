e5435b9a73b429628b886989ddcb62e095a39db0
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-09-30 21:40:03 +0545

    create a custom user model using abstract user and making it default

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    class GENDER(models.TextChoices):
+        MALE = "MALE", 'MALE'
+        FEMALE = "FEMALE", 'FEMALE'
+        OTHERS = "OTHERS", 'OTHERS'
+
+
+    age = models.PositiveIntegerField(blank = True, null=True)
+    gender = models.CharField(max_length=10, choices=GENDER.choices, blank=True, null=True)
