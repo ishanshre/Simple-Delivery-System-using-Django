2e453835e954a60eb7b5a81117780742642392e0
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-11-01 09:32:40 +0545

    feat: send message to email upon user creation

--- accounts/signals.py
+++ accounts/signals.py
@@ -2,7 +2,8 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.contrib.auth import get_user_model
 from .models import Profile
-
+from django.core.mail import send_mail
+from django.conf import settings
 User = get_user_model()
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
@@ -12,3 +13,9 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.user_profile.save()
+
+
+@receiver(post_save, sender=User)
+def send_welcome_mail(sender, instance, created, **kwargs):
+    if created and instance.email:
+        send_mail('Welcome to Delevery SYstem', f"Hello {instance.username}! Welcome to Best deleviry System", settings.DEFAULT_FROM_EMAIL, [instance.email])
--- core/settings.py
+++ core/settings.py
@@ -159,3 +159,5 @@ RECAPTCHA_PUBLIC_KEY = env('RECAPTCHA_PUBLIC_KEY')
 RECAPTCHA_PRIVATE_KEY = env('RECAPTCHA_PRIVATE_KEY')
 SILENCED_SYSTEM_CHECKS = ['captcha.recaptcha_test_key_error']
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'Deleviry System <no-reply@deleviry.localhost>'
